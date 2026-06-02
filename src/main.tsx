d47d98145993eb3c8b49f23a8ce6dffce7c129ea
Guðjón Karl Arnarson <kromby@hress.org>
2026-06-02 21:16:32 +0000

    feat: wire auth routes and provider into the app

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { AuthProvider } from "./auth/AuthProvider";
 import { createQueryClient } from "./query/queryClient";
 import "./styles/app.css";
 
@@ -12,7 +13,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
